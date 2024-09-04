0deb0064b21ccf654ec5c0dc4b2997006c109886
pod806 <slugwolf04@gmail.com>
2024-09-04 15:14:10 +0300

    4.rs

--- src/4.rs
+++ src/4.rs
@@ -1,29 +1,24 @@
-fn is_palindrome_number(number: i32) -> bool {                                            //4.
-    if number < 0 {
-        return false;
-    }
+use std::io::{self, Write};         // O(n) karmaşıklığında yapabildim
 
-    let mut original_number = number;
-    let mut reversed_number = 0;
-
-    while original_number != 0 {
-        let remainder = original_number % 10;
-        reversed_number = reversed_number * 10 + remainder;
-        original_number /= 10;
-    }
-
-    if reversed_number == number {
-        true
-    } else {
-        false
-    }
+fn is_palindrome_number(number: i32) -> bool {
+    let num_str = number.to_string();
+    num_str.chars().eq(num_str.chars().rev())
 }
 
 fn main() {
-    let number = 5353535;
-    if is_palindrome_number(number) {
-        println!("{} palindrom sayıdır.", number);
+    print!("Bir sayı girin: ");
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Okuma hatası");
+
+    if let Ok(number) = input.trim().parse::<i32>() {
+        if is_palindrome_number(number) {
+            println!("{} palindrom sayıdır.", number);
+        } else {
+            println!("{} palindrom sayı değildir.", number);
+        }
     } else {
-        println!("{} palindrom sayı değildir.", number);
+        println!("Geçerli bir sayı giriniz.");
     }
 }
