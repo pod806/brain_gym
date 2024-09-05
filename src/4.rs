97481dda8b9939190ec414863b78609e67c6e412
pod806 <slugwolf04@gmail.com>
2024-09-05 14:17:12 +0300

    Update 4.rs

--- src/4.rs
+++ src/4.rs
@@ -1,24 +1,25 @@
-use std::io::{self, Write};         // O(n) karmaşıklığında yapabildim.
+use std::io;
 
-fn is_palindrome_number(number: i32) -> bool {
-    let num_str = number.to_string();
-    num_str.chars().eq(num_str.chars().rev())
+fn palindrome(num: i32) -> bool {
+    if num < 0 {
+        return false; 
+    }
+
+    let num_str = num.to_string();
+    let reversed: String = num_str.chars().rev().collect();
+    num_str == reversed
 }
 
 fn main() {
-    print!("Bir sayı girin: ");
-    io::stdout().flush().unwrap();
-
+    println!("Bir sayı girin:");
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Okuma hatası");
+    io::stdin().read_line(&mut input).expect("Girdi okunamadı");
+    
+    let num: i32 = input.trim().parse().expect("Geçerli bir sayı gir");
 
-    if let Ok(number) = input.trim().parse::<i32>() {
-        if is_palindrome_number(number) {
-            println!("{} palindrom sayıdır.", number);
-        } else {
-            println!("{} palindrom sayı değildir.", number);
-        }
+    if palindrome(num) {
+        println!("{} palindrom sayıdır.", num);
     } else {
-        println!("Geçerli bir sayı giriniz.");
+        println!("{} palindrom sayı değildir.", num);
     }
 }
